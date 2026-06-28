5ad392e7ff769db186ee8f56d8a6789eddaa90ae
Rajal Suwal <suwalrajal57@gmail.com>
2026-06-28 22:07:44 +0900

    useSession hook from next-auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+
+import React from "react";
+import { useSession } from "next-auth/react";
+
 
 export default function Home() {
+const {data: session} = useSession()
+
   return (
-   <div>home</div>
+   <div> {JSON.stringify(session)}  home</div>
   );
 }
